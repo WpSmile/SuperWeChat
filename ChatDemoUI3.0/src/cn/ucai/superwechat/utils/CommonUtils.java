7a080c5e95dba3da57ae8f733d2eecb4268d8336
WpSmile <179292126@qq.com>
2016-11-04 15:29:44 +0800

    将含Demo前缀名的修改为SuperWeChat

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/CommonUtils.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/CommonUtils.java
@@ -2,19 +2,19 @@ package cn.ucai.superwechat.utils;
 
 import android.widget.Toast;
 
-import cn.ucai.superwechat.DemoApplication;
+import cn.ucai.superwechat.SuperWeChatApplication;
 
 public class CommonUtils {
     public static void showLongToast(String msg){
-        Toast.makeText(DemoApplication.getInstance(),msg,Toast.LENGTH_LONG).show();
+        Toast.makeText(SuperWeChatApplication.getInstance(),msg,Toast.LENGTH_LONG).show();
     }
     public static void showShortToast(String msg){
-        Toast.makeText(DemoApplication.getInstance(),msg,Toast.LENGTH_SHORT).show();
+        Toast.makeText(SuperWeChatApplication.getInstance(),msg,Toast.LENGTH_SHORT).show();
     }
     public static void showLongToast(int rId){
-        showLongToast(DemoApplication.getInstance().getString(rId));
+        showLongToast(SuperWeChatApplication.getInstance().getString(rId));
     }
     public static void showShortToast(int rId){
-        showShortToast(DemoApplication.getInstance().getString(rId));
+        showShortToast(SuperWeChatApplication.getInstance().getString(rId));
     }
 }

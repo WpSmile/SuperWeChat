fb72a09d182564cdc2b2b495eac1411753e06e82
WpSmile <179292126@qq.com>
2016-11-08 14:33:59 +0800

    添加的好友显示最新的头像，并按时间先后排序

--- ChatDemoUI3.0/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
@@ -107,7 +107,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 						Result result = ResultUtils.getResultFromJson(s, User.class);
 						if (result!=null&&result.isRetMsg()){
 							User u = (User) result.getRetData();
-							EaseUserUtils.setAppUserAvatar(context,msg.getFrom(),holder.avator);
+							EaseUserUtils.setAppUserPathAvatar(context,u.getAvatar(),holder.avator);
 							EaseUserUtils.setAppUserNick(u.getMUserNick(),holder.name);
 						}
 					}
@@ -227,8 +227,10 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 							buttonAgree.setText(str2);
 							buttonAgree.setBackgroundDrawable(null);
 							buttonAgree.setEnabled(false);
-							
+							buttonAgree.setVisibility(View.GONE);
 							buttonRefuse.setVisibility(View.INVISIBLE);
+							buttonRefuse.setText(str2);
+							buttonRefuse.setBackgroundDrawable(null);
 						}
 					});
 				} catch (final Exception e) {
